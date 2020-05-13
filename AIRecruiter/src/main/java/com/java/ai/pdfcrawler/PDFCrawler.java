f2314e7143ffb4d5edcd52f9620118437328c96e
S-Akhilesh <toakhilesh48@gmail.com>
2020-05-13 14:50:37 +0530

    Crawler with jsoup added

--- AIRecruiter/src/main/java/com/java/ai/pdfcrawler/PDFCrawler.java
+++ AIRecruiter/src/main/java/com/java/ai/pdfcrawler/PDFCrawler.java
@@ -1,6 +1,7 @@
 package com.java.ai.pdfcrawler;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -12,9 +13,15 @@ public class PDFCrawler {
 	public static void main(String[] args) {
 		
 		Document doc = null;
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter the url");		
+		String url = input.next();
+		
+		url += "http://" + url;
 		
 		try {
-			doc = Jsoup.connect("http://www.ssaurel.com/blog").get();
+			doc = Jsoup.connect(url).get();
 			String title = doc.title();
 			System.out.println("Title : " + title);
 			
